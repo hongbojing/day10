10e90336f6765a8f4d4d698c3ca7cf1c089f2101
Hongbo Jing <jack.jing7@gmail.com>
2015-07-05 09:42:27 -0400

    day 10 dev environment set up

---
+++ builds/development/js/lib/set.js
@@ -0,0 +1,26 @@
+
+
+
+var paper40=Raphael("code-left-40",400,400);
+
+
+var paper41=Raphael("code-left-41",400,400);
+
+
+var paper42=Raphael("code-left-42",400,400);
+
+
+var paper43=Raphael("code-left-43",400,400);
+
+
+var paper44=Raphael("code-left-44",400,400);
+
+
+var paper45=Raphael("code-left-45",400,400);
+
+
+
+
+
+
+
